3717baea203d74af2c7339a902e24a0ab3e9555a
Ing. Jaime Agudelo <ingenieroagudelo@gmail.com>
2022-04-23 10:08:24 -0500

    README  agregado y editado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # curso-node
+
 Ejercicios de las clases en NodeJS - Udemy
+
+```
+Opciones:
+--help Muestra ayuda [booleano]
+--version Muestra número de versión [booleano]
+-b, --base Es la base de la tabla de multiplicar [número] [requerido]
+-l, --listar Determina si muestra la lista o no en consola 'true/false'
+[booleano] [defecto: false]
+-h, --hasta Numero hasta donde va la tabla de multiplicar
+[número] [defecto: 10]
+```
--- config/yargs.js
+++ config/yargs.js
@@ -3,16 +3,19 @@ const argv = require("yargs")
     alias: "base",
     type: "number",
     demandOption: true,
+    description: "Es la base de la tabla de multiplicar",
   })
   .option("l", {
     alias: "listar",
     type: "boolean",
     default: false,
+    description: "Determina si muestra la lista o no en consola 'true/false'",
   })
   .option("h", {
     alias: "hasta",
     type: "number",
     default: 10,
+    description: "Numero hasta donde va la tabla de multiplicar",
   })
   .check((argv, options) => {
     if (isNaN(argv.b)) {
